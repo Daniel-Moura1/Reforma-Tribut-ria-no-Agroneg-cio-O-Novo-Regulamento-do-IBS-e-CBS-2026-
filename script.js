3c6736c6dad73285500d9ac6db1664c06dfd9fcc
Daniel-Moura1 <daniel.moura@horita.com.br>
2026-04-29 11:18:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// FAQ accordion
+document.querySelectorAll('.faq-item').forEach(item => {
+  item.querySelector('.faq-q').addEventListener('click', () => {
+    const wasOpen = item.classList.contains('open');
+    document.querySelectorAll('.faq-item').forEach(i => i.classList.remove('open'));
+    if (!wasOpen) item.classList.add('open');
+  });
+});
+// Abre o primeiro por padrão
+document.querySelector('.faq-item')?.classList.add('open');
